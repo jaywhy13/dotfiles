b6b9ed8de5e5372b7b73acd9c22796993a6f6e77
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-07-12 12:50:26 -0400

    Fixed borked comment that was causing the configuration not to load.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -21,7 +21,7 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
--
+--
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
