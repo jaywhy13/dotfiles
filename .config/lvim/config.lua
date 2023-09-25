34e07c7ff702b40a205ccada94e114d2c24a221f
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-09-24 22:56:03 -0400

    Remove Neoformat configuration to format before saving
    I can now safely remove the Neoformat configuration. I had
    a trailing comma in my prettier configuration. I confirmed
    the problem by running "yarn run pretter src". That surfaced
    the problem. Once the configuration is OK, then prettier
    will nicely format the code as expected.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -412,13 +412,3 @@ lspconfig.pyright.setup {
     }
   }
 }
-
-
--- Configure Neoformat
--- Try to look for Node locally
--- vim.g.neoformat_try_node_exe = 1
--- -- Call Neoformat before files are saved
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
---   command = "Neoformat",
--- })
