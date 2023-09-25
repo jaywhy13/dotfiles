62a16749f85b6952bcc78938aca286e7da7675a4
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-09-24 22:41:35 -0400

    Telescope bindings for projects
    I haven't used this yet.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -40,8 +40,6 @@ lvim.builtin.telescope.defaults.mappings = {
   },
 }
 
--- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
@@ -352,6 +350,9 @@ lvim.builtin.which_key.vmappings["ai"] = {
 -- Change theme settings
 -- lvim.builtin.theme.options.dim_inactive = true
 -- lvim.builtin.theme.options.style = "storm"
+
+-- Use which-key to add extra bindings with the leader-key prefix
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
   name = "+Trouble",
   r = { "<cmd>Trouble lsp_references<cr>", "References" },
