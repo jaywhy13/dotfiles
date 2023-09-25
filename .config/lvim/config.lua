571c02b86b668d1c96ecd1132f4c6d263231d2ff
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-09-24 22:03:26 -0400

    Added some configuration for IronREPL.
    I think I added this when trying to set up the terminal.
    I'm not sure if I need it.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -231,6 +231,55 @@ require('ufo').setup()
 --   end,
 -- })
 
+-- Configuring Iron REPL
+local iron = require("iron.core")
+
+iron.setup {
+  config = {
+    -- Whether a repl should be discarded or not
+    scratch_repl = true,
+    -- Your repl definitions come here
+    repl_definition = {
+      sh = {
+        -- Can be a table or a function that
+        -- returns a table (see below)
+        command = { "zsh" }
+      }
+    },
+    -- How the repl window will be displayed
+    -- See below for more information
+    repl_open_cmd = "vertical botright 80 split"
+  },
+  -- Iron doesn't set keymaps by default anymore.
+  -- You can set them here or manually add keymaps to the functions in iron.core
+  keymaps = {
+    send_motion = "<space>Rc",
+    visual_send = "<space>Rc",
+    send_file = "<space>Rf",
+    send_line = "<space>Rl",
+    send_until_cursor = "<space>Ru",
+    send_mark = "<space>Rm",
+    mark_motion = "<space>mc",
+    mark_visual = "<space>mc",
+    remove_mark = "<space>md",
+    cr = "<space>R<cr>",
+    interrupt = "<space>R<space>",
+    exit = "<space>Rq",
+    clear = "<space>cl",
+  },
+  -- If the highlight is on, you can change how it looks
+  -- For the available options, check nvim_set_hl
+  highlight = {
+    italic = true
+  },
+  ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+}
+
+-- iron also has a list of commands, see :h iron-commands for all available commands
+vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
 
 -- Adding some of my customizations here
 lvim.keys.normal_mode["<esc>"] = false
